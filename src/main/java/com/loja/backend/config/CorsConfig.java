cd970bd8eb9ca4e51680dffc8d215d6f8f0ddf99
Arthur <arthurgomees03@gmail.com>
2025-12-03 14:53:37 -0300

    Corrigindo erros de api

--- src/main/java/com/loja/backend/config/CorsConfig.java
+++ src/main/java/com/loja/backend/config/CorsConfig.java
@@ -15,7 +15,10 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200",
+            "https://lojapshelby.vercel.app"
+        ));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
@@ -25,5 +28,3 @@ public class CorsConfig {
         return new CorsFilter(source);
     }
 }
-
-
